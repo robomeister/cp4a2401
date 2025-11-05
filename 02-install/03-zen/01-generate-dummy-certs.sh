00209caf0707e6d82ebdcd9e2f41b3ec849521e5
root <root@jumpserver2.robobob.ca>
2025-11-05 08:58:58 -0600

    updates

---
+++ 02-install/03-zen/01-generate-dummy-certs.sh
@@ -0,0 +1,2 @@
+oc create secret generic dummy-tls-cert-1 --from-literal=tls.key= --from-literal=tls.crt= --from-literal=ca.crt=
+oc create secret generic dummy-tls-cert-2 --from-literal=tls.key= --from-literal=tls.crt= --from-literal=ca.crt=
